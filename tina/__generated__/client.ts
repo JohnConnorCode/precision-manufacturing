6d9c0eb593cb5c4447c5ca9a5f414344dc26e88f
John Connor <johnconnorchat@gmail.com>
2025-10-29 20:01:22 +0800

    Add generated Tina schema files for TinaCloud indexing
    TinaCloud needs the generated schema files to index branches.
    These files define the GraphQL schema and content structure.

---
+++ tina/__generated__/client.ts
@@ -0,0 +1,5 @@
+import { createClient } from "tinacms/dist/client";
+import { queries } from "./types";
+export const client = createClient({ cacheDir: '/Users/johnconnor/Documents/GitHub/iismet/precision-manufacturing/tina/__generated__/.cache/1761738372199', url: 'https://content.tinajs.io/1.6/content/1776d518-92cc-4faf-acd9-2686d5a1f7e0/github/main', token: 'a2e9e2e759484e4aa0b894e1c8a0a920e19b14e2', queries,  });
+export default client;
+  
